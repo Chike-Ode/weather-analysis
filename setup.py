168fc7bac613cfebb311c78751fd896b50a613e4
Chike Odenigbo <chikeodenigbo@gmail.com>
2021-04-25 09:09:17 -0400

    initial commit created dataset for montreal

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project seeks to predict weather data through time series methodologies',
+    author='Chike Odenigbo',
+    license='',
+)
